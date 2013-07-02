8ed0668f1091818c642965f16c730b8097780c25
Ben Corman <corman@lo-tek.org>
2013-07-02 16:46:44 -0400

    Done: Set tags by marks, not by absolute positions (marks travel with text) Get tag position by tag_ranges(tag), not mark + len(word) (word length changes) Check if work is already tagged as error tag_names(idx) - Shouldn't double tag the word

--- tkspellcheck.py
+++ tkspellcheck.py
@@ -6,29 +6,31 @@ from enchant.checker import SpellChecker
 
 class tkSpellCheck:
     def __init__(self, textwidget, lang='en_US'):
-        self.textwidget = textwidget
+        self.txtwdgt = textwidget
         self.suggwords = enchant.Dict(lang)
         self.checker = SpellChecker(lang)
         self.spellerrs = {}
 
-    def __call__(self, mode=False):
-        if mode == 'fullcheck':
-            self.fullcheck()
-        if mode == 'inline':
-            self.inlinecheck()
-        if not mode:
-            self.remove_tags()
+    def __call__(self, disable=False):
+        if disable:
+            self.__cleartags()
+            self.__clearbindings()
+        else:
+            self.__setup()
 
-    def fullcheck(self):
-        self.__clearalltags()
+    def __setup(self):
+        self.__fullcheck()
+        self.txtwdgt.bind('<space>', self.__spellcheck())
+
+    def __fullcheck(self):
+        self.__cleartags()
         text = self.__alltxt()
-        self.__settags(text, startpos='1.0', endpos='END')
+        self.__settags(text, 1.0, END)
 
-    def inlinecheck(self):
-        startpos = self.textwidget.index('insert linestart')
-        endpos = self.textwidget.index('insert lineend')
+    def __spellcheck(self):
+        startpos = self.txtwdgt.index('insert linestart')
+        endpos = self.txtwdgt.index('insert lineend')
         text = self.__linetxt(startpos, endpos)
-        self.__clearlntags(currln=startpos)
         self.__settags(text, startpos=startpos, endpos=endpos)
 
     def __settags(self, text, startpos, endpos):
@@ -36,56 +38,62 @@ class tkSpellCheck:
         curpos = startpos
         self.checker.set_text(text)
         for err in self.checker:
-            currln = self.textwidget.index('insert linestart')
-            tag = '{0}-{1}.{2}'.format(err.word, currln, i)
-            mrk = 'mrk-{0}'.format(tag)
-            firstltr = self.textwidget.search(err.word, curpos, endpos)
+            firstltr = self.txtwdgt.search(err.word, curpos, endpos)
             lastltr = firstltr + ('+%dchars' % len(err.word))
+            if not self.__istagged(firstltr):
+                tag = '{0}-{1}'.format(err.word, i)
+                markleft = 'ml-{0}'.format(tag)
+                markright = 'ml-{0}'.format(tag)
+                self.txtwdgt.tag_config(tag, foreground="red", underline=True)
+                self.txtwdgt.mark_set(markleft, firstltr)
+                self.txtwdgt.mark_set(markright, lastltr)
+                self.txtwdgt.mark_gravity(markleft, LEFT)
+                self.txtwdgt.mark_gravity(markright, RIGHT)
+                self.txtwdgt.tag_add(tag, markleft, markright)
+                self.txtwdgt.tag_bind(tag, '<Button-3>', lambda evt,
+                                      tag=tag: self.__suggestedwords(evt, tag))
+                i += 1
+                self.spellerrs[tag] = (err.word, markleft, markright)
+            curpos = lastltr
 
-            self.textwidget.tag_config(tag, foreground="red", underline=True)
-            self.textwidget.tag_add(tag, firstltr, lastltr)
-            self.textwidget.mark_set(mrk, firstltr)
-            self.textwidget.tag_bind(tag, '<Button-3>', lambda event,
-                                     tag=tag: self.__suggestedwords(event, tag))
-            i += 1
-            curpos = last_ltr
-            self.spellerrs[tag] = (err.word, mrk, currln)
+    def __istagged(self, idx):
+        tag = self.txtwdgt.tag_names(idx)
+        if tag in self.spellerrs.keys():
+            return True
+        else:
+            return False
 
-    def __suggestedwords(self, event, tag):
+    def __suggestedwords(self, evt, tag):
         err = self.spellerrs[tag][0]
         suggwords = self.__suggest(err)
         context_menu = self.__contextmenu(tag, suggwords)
-        context_menu.tk_popup(event.x_root, event.y_root)
+        context_menu.tk_popup(evt.x_root, evt.y_root)
 
     def __clearalltags(self):
-        if self.spellerrs:
-            for tag in self.spellerrs.keys():
-                self.__unsettag(self, tag)
-            self.spellerrs = {}  # Reset the dictionary
+        for tag in self.spellerrs.keys():
+            self.__deltag(tag)
 
-    def __clearlntags(self, currln):
-        for tag, val in self.spellerrs.items():
-            if val[2] == currln:
-                self.__unsettag(self, tag)
-                del self.spellerrs[tag]
+    def __clearbindings(self):
+        self.txtwdgt.bind('<space>', lambda e: None)
 
-    def __unsettag(self, tag):
-        mrk = self.spellerrs[tag][1]
-        self.textwidget.tag_delete(tag)
-        self.textwidget.mark_unset(mrk)
+    def __deltag(self, tag):
+        self.txtwdgt.tag_delete(tag)
+        for i in (self.spellerrs[tag][1], self.spellerrs[tag][2]):
+            self.txtwdgt.mark_unset(i)
+        del self.spellerrs[tag]
 
     def __alltxt(self):
-        return self.textwidget.get(1.0, END)
+        return self.txtwdgt.get(1.0, END)
 
     def __linetxt(self, startpos, endpos):
-        return self.textwidget.get(startpos, endpos)
+        return self.txtwdgt.get(startpos, endpos)
 
     def __suggest(self, err):
         return self.suggwords.suggest(err)
 
     def __contextmenu(self, tag, suggested):
         err = self.spellerrs[tag][0]
-        contextmenu = Menu(self.textwidget, tearoff=False)
+        contextmenu = Menu(self.txtwdgt, tearoff=False)
         for word in suggested:
             contextmenu.add_command(label=word, command=lambda tag=tag,
                                     word=word: self.__replace(tag, word))
@@ -94,16 +102,15 @@ class tkSpellCheck:
                                 err=err: self.__usrdict(err))
         return contextmenu
 
+    def __tagranges(self, tag):
+        return self.txtwdgt.tag_ranges(tag)
+
     def __replace(self, tag, word):
-        mrk = self.spellerrs[tag][1]
+        firstltr, lastltr = self.__tagranges(tag)
         err = self.spellerrs[tag][0]
-        firstltr = self.textwidget.index(mrk)
-        lastltr = firstltr + ('+%dchars' % len(err))
-        self.textwidget.delete(firstltr, lastltr)
-        self.textwidget.insert(firstltr, word)
-        self.textwidget.tag_delete(tag)
-        self.textwidget.mark_unset(mrk)
-        del self.spellerrs[tag]
+        self.txtwdgt.delete(firstltr, lastltr)
+        self.txtwdgt.insert(firstltr, word)
+        self.__deltag(tag)
 
     def __usrdict(self, err):
         '''TODO: Add a personal dictionary file '''
