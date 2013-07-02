9fe989af6b8b2193fb61d1053b1aa7ed9deead72
Ben Corman <corman@lo-tek.org>
2013-07-02 10:11:34 -0400

    all functions written(i think?)

--- tkspellcheck.py
+++ tkspellcheck.py
@@ -7,83 +7,105 @@ from enchant.checker import SpellChecker
 class tkSpellCheck:
     def __init__(self, textwidget, lang='en_US'):
         self.textwidget = textwidget
-        self.swords = enchant.Dict(lang)        # S(uggested) Words, obviously
+        self.suggwords = enchant.Dict(lang)
         self.checker = SpellChecker(lang)
-        self.sp_err = {}
+        self.spellerrs = {}
 
     def __call__(self, mode=False):
-        if mode == 'init':
+        if mode == 'fullcheck':
             self.fullcheck()
         if mode == 'inline':
             self.inlinecheck()
         if not mode:
-            self.destroy_tags()
-
-    def destroy_tags(self):
-        if self.sp_err:
-            for tag in self.sp_err.keys():
-                mrk = self.sp_err[tag][1]
-                self.textwidget.tag_delete(tag)
-                self.textwidget.mark_unset(mrk)
-            self.sp_err = {}  # Reset the dictionary for next time
+            self.remove_tags()
 
     def fullcheck(self):
-        self.destroy_tags()
+        self.__clearalltags()
         text = self.__alltxt()
-        self.checker.set_text(text)
-        i = 1
-        curpos = 1.0
+        self.__settags(text, startpos='1.0', endpos='END')
 
+    def inlinecheck(self):
+        startpos = self.textwidget.index('insert linestart')
+        endpos = self.textwidget.index('insert lineend')
+        text = self.__linetxt(startpos, endpos)
+        self.__clearlntags(currln=startpos)
+        self.__settags(text, startpos=startpos, endpos=endpos)
+
+    def __settags(self, text, startpos, endpos):
+        i = 1
+        curpos = startpos
+        self.checker.set_text(text)
         for err in self.checker:
-            tag = '{0}-{1}'.format(err.word, i)
+            currln = self.textwidget.index('insert linestart')
+            tag = '{0}-{1}.{2}'.format(err.word, currln, i)
             mrk = 'mrk-{0}'.format(tag)
-            first_ltr = self.textwidget.search(err.word, curpos, END)
-            last_ltr = first_ltr + ('+%dchars' % len(err.word))
+            firstltr = self.textwidget.search(err.word, curpos, endpos)
+            lastltr = firstltr + ('+%dchars' % len(err.word))
 
             self.textwidget.tag_config(tag, foreground="red", underline=True)
-            self.textwidget.tag_add(tag, first_ltr, last_ltr)
-            self.textwidget.mark_set(mrk, first_ltr)
+            self.textwidget.tag_add(tag, firstltr, lastltr)
+            self.textwidget.mark_set(mrk, firstltr)
             self.textwidget.tag_bind(tag, '<Button-3>', lambda event,
-                                     tag=tag: self.show_suggestions(event, tag))
+                                     tag=tag: self.__suggestedwords(event, tag))
             i += 1
             curpos = last_ltr
-            self.sp_err[tag] = (err.word, mrk)
+            self.spellerrs[tag] = (err.word, mrk, currln)
 
-    def show_suggestions(self, event, tag):
-        err = self.sp_err[tag][0]
-        suggested_words = self.__suggest(err)
-        context_menu = self.__build_cnxtmnu(tag, suggested_words)
-        print event.x_root, event.y_root
+    def __suggestedwords(self, event, tag):
+        err = self.spellerrs[tag][0]
+        suggwords = self.__suggest(err)
+        context_menu = self.__contextmenu(tag, suggwords)
         context_menu.tk_popup(event.x_root, event.y_root)
+
+    def __clearalltags(self):
+        if self.spellerrs:
+            for tag in self.spellerrs.keys():
+                self.__unsettag(self, tag)
+            self.spellerrs = {}  # Reset the dictionary
+
+    def __clearlntags(self, currln):
+        for tag, val in self.spellerrs.items():
+            if val[2] == currln:
+                self.__unsettag(self, tag)
+                del self.spellerrs[tag]
+
+    def __unsettag(self, tag):
+        mrk = self.spellerrs[tag][1]
+        self.textwidget.tag_delete(tag)
+        self.textwidget.mark_unset(mrk)
 
     def __alltxt(self):
         return self.textwidget.get(1.0, END)
 
-    def __suggest(self, err):
-        return self.swords.suggest(err)
+    def __linetxt(self, startpos, endpos):
+        return self.textwidget.get(startpos, endpos)
 
-    def __build_cnxtmnu(self, tag, suggested):
-        err = self.sp_err[tag][0]
-        cnxtmnu = Menu(self.textwidget, tearoff=False)
+    def __suggest(self, err):
+        return self.suggwords.suggest(err)
+
+    def __contextmenu(self, tag, suggested):
+        err = self.spellerrs[tag][0]
+        contextmenu = Menu(self.textwidget, tearoff=False)
         for word in suggested:
-            cnxtmnu.add_command(label=word, command=lambda tag=tag,
-                                  word=word: self.__replace(tag,word))
-        cnxtmnu.add_separator()
-        cnxtmnu.add_command(label='Add to Dictionary', command=lambda
-                            err=err: self.__usr_def(err))
-        return cnxtmnu
+            contextmenu.add_command(label=word, command=lambda tag=tag,
+                                    word=word: self.__replace(tag, word))
+        contextmenu.add_separator()
+        contextmenu.add_command(label='Add to Dictionary', command=lambda
+                                err=err: self.__usrdict(err))
+        return contextmenu
 
     def __replace(self, tag, word):
-        mrk = self.sp_err[tag][1]
-        err = self.sp_err[tag][0]
-        first_ltr = self.textwidget.index(mrk)
-        last_ltr = first_ltr + ('+%dchars' % len(err))
-        self.textwidget.delete(first_ltr, last_ltr)
-        self.textwidget.insert(first_ltr, word)
+        mrk = self.spellerrs[tag][1]
+        err = self.spellerrs[tag][0]
+        firstltr = self.textwidget.index(mrk)
+        lastltr = firstltr + ('+%dchars' % len(err))
+        self.textwidget.delete(firstltr, lastltr)
+        self.textwidget.insert(firstltr, word)
         self.textwidget.tag_delete(tag)
         self.textwidget.mark_unset(mrk)
+        del self.spellerrs[tag]
 
-    def __usr_def(self, err):
+    def __usrdict(self, err):
         '''TODO: Add a personal dictionary file '''
         pass
 
