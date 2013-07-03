d8f0cf9b4e8ef2aceae1d45af8eb62fc4413cad2
Ben Corman <corman@lo-tek.org>
2013-07-03 15:37:11 -0400

    To do:
    1. Figure out which events to bind to for spellcheck and for corrections.
    2. Fix problem where deleting or adding a letter in the middle of the word
    causes it to be considered as two words
    3. Figure out how to parse sections of a document at a time (for performance)
    
    Should check_full_document and inline_check be separate functions / classes?

--- tkspellcheck.py
+++ tkspellcheck.py
@@ -1,5 +1,4 @@
 from Tkinter import *
-from ttk import *
 import enchant
 from enchant.checker import SpellChecker
 
@@ -8,36 +7,71 @@ class tkSpellCheck(Text):
     def __init__(self, master=None, cnf={}, **kw):
         Text.__init__(self, master, cnf, **kw)
 
-    def spellcheck(self, lang='en_US', usrdict=[]):
+    def enable_inline_spellcheck(self, lang='en_US', usrdict=[]):
         self.suggwords = enchant.Dict(lang)
         self.checker = SpellChecker(lang)
         self.spellingerrors = {}
+        self.counter = 0
         self.__initialcheck()
 
-    def fullcheck(self):
+    def full_document_spellcheck(self, lang='en_US', usrdict=[]):
         self.__cleartags()
-        text = self.__alltxt()
+        text = self.__gettxt()
         self.__settags(text, 1.0, END)
 
-    def __initialcheck(self):
-        self.fullcheck()
-        self.bind('<space>', self.__check)
+    def disable_inline_spellcheck(self):
+        self.__cleartags()
+        self.__clearbindings()
 
-    def __check(self, evt):
+    def clear_document(self):
+        self.__cleartags()
+        self.__clearbindings()      # Just in Case
+
+    def __initialcheck(self):
+        text = self.__gettxt()
+        self.__settags(text, 1.0, END)
+        self.bind('<space>', self.__checkline)
+        self.bind('<Key>', self.__checkcorrections)
+
+    def __checkcorrections(self, evt):
+        for tag in self.spellingerrors.keys():
+            firstltr, lastltr = self.__tagranges(tag)
+            text = self.__gettxt(firstltr, lastltr)
+            if text != self.spellingerrors[tag][0]:
+                self.__deltag(tag)
+                self.__settags(text, firstltr, lastltr)
+
+    def __checkline(self, evt):
+        print self.counter
+        self.__del_existing_tags(INSERT)
         startpos = self.index('insert linestart')
         endpos = self.index('insert lineend')
-        text = self.__linetxt(startpos, endpos)
-        self.__settags(text, startpos=startpos, endpos=endpos)
+        text = self.__gettxt(startpos, endpos)
+        self.__settags(text, startpos, endpos)
+
+    def __del_existing_tags(self, idx):
+        taglist = self.__istagged(idx)
+        for tag in taglist:
+            if tag in self.spellingerrors.keys():
+                self.__deltag(tag)
+
+    def __check_existing_tags(self, idx):
+        taglist = self.__istagged(idx)
+        for tag in taglist:
+            if tag in self.spellingerrors.keys():
+                return True
+            else:
+                return False
 
     def __settags(self, text, startpos, endpos):
-        i = 1
         curpos = startpos
         self.checker.set_text(text)
         for err in self.checker:
             firstltr = self.search(err.word, curpos, endpos)
             lastltr = firstltr + ('+%dchars' % len(err.word))
-            if not self.__istagged(firstltr):
-                tag = '{0}-{1}'.format(err.word, i)
+            found = self.__check_existing_tags(firstltr)
+            if not found:
+                tag = '{0}-{1}'.format(err.word, self.counter)
                 markleft = 'ml-{0}'.format(tag)
                 markright = 'mr-{0}'.format(tag)
                 self.tag_config(tag, foreground="red", underline=True)
@@ -49,16 +83,14 @@ class tkSpellCheck(Text):
                 self.tag_add(tag, firstltr, lastltr)
                 self.tag_bind(tag, '<Button-3>', lambda evt,
                               tag=tag: self.__suggestedwords(evt, tag))
-                i += 1
-                self.spellingerrors[tag] = (err.word, markleft, markright)
+
+                self.spellingerrors[tag] = (err.word, markleft, markright,
+                                            self.counter)
+                self.counter += 1
             curpos = lastltr
 
     def __istagged(self, idx):
-        tag = self.tag_names(idx)
-        if tag in self.spellingerrors.keys():
-            return True
-        else:
-            return False
+        return self.tag_names(idx)
 
     def __suggestedwords(self, evt, tag):
         err = self.spellingerrors[tag][0]
@@ -69,6 +101,7 @@ class tkSpellCheck(Text):
     def __cleartags(self):
         for tag in self.spellingerrors.keys():
             self.__deltag(tag)
+        self.counter = 0
 
     def __clearbindings(self):
         self.bind('<space>', lambda e: None)
@@ -79,10 +112,7 @@ class tkSpellCheck(Text):
             self.mark_unset(i)
         del self.spellingerrors[tag]
 
-    def __alltxt(self):
-        return self.get(1.0, END)
-
-    def __linetxt(self, startpos, endpos):
+    def __gettxt(self, startpos=1.0, endpos=END):
         return self.get(startpos, endpos)
 
     def __suggest(self, err):
