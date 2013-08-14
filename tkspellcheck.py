55f936cf3a7488b63f80e34e302cf7a3596fa1a5
Ben Corman <corman@lo-tek.org>
2013-08-14 00:33:45 -0400

    Works for notes (no huge novels). Singles on the text.widget <<Modify>> event.

--- tkspellcheck.py
+++ tkspellcheck.py
@@ -1,146 +1,118 @@
-from Tkinter import *
+from Tkinter import Text, Menu, RIGHT, LEFT, END
+from ttk import *
 import enchant
 from enchant.checker import SpellChecker
+from enchant.tokenize import EmailFilter, URLFilter, HTMLChunker
+import time
 
 
-class tkSpellCheck(Text):
+class AlertMixin(object):
+
+    def initialize_alerts(self):
+        self.reset_flag()
+        self.bind('<<Modified>>', self._textevent)
+
+    def _textevent(self, event=None):
+        if not self._flag_reset:
+            self.reset_flag()
+            self.fire_on_alert(event)
+
+    def fire_on_alert(self, event=None):
+        pass
+
+    def reset_flag(self):
+        """http://epydoc.sourceforge.net/stdlib/Tkinter.Text-class.html#edit_modified"""
+        self._flag_reset = True
+        self.edit_modified(False)
+        self._flag_reset = False
+
+
+class CkTxt(AlertMixin, Text):
     def __init__(self, master=None, cnf={}, **kw):
         Text.__init__(self, master, cnf, **kw)
 
-    def enable_inline_spellcheck(self, lang='en_US', usrdict=[]):
-        self.suggwords = enchant.Dict(lang)
-        self.checker = SpellChecker(lang)
+    def enable(self, lang='en_US'):
         self.spellingerrors = {}
+        self.suggest_words = enchant.Dict(lang)
+        self.checker = SpellChecker(lang, filters=[EmailFilter, URLFilter], chunkers=[HTMLChunker])
         self.counter = 0
-        self.__initialcheck()
+        self._firstcheck()
+        self.initialize_alerts()
 
-    def full_document_spellcheck(self, lang='en_US', usrdict=[]):
-        self.__cleartags()
-        text = self.__gettxt()
-        self.__settags(text, 1.0, END)
+    def disable(self):
+        self._clear_tags()
+        self._clearbindings()
 
-    def disable_inline_spellcheck(self):
-        self.__cleartags()
-        self.__clearbindings()
+    def _clearbindings(self):
+        self.bind('<<Modified>>', lambda e: None)
 
-    def clear_document(self):
-        self.__cleartags()
-        self.__clearbindings()      # Just in Case
+    def fire_on_alert(self, event=None):
+        start = time.time()
+        self.after_idle(self._fire)
+        ttime = (time.time() - start)
+        print ttime
 
-    def __initialcheck(self):
-        text = self.__gettxt()
-        self.__settags(text, 1.0, END)
-        self.bind('<space>', self.__checkline)
-        self.bind('<Key>', self.__checkcorrections)
+    def _fire(self):
+        self._clear_tags()
+        self._check()
 
-    def __checkcorrections(self, evt):
-        for tag in self.spellingerrors.keys():
-            firstltr, lastltr = self.__tagranges(tag)
-            text = self.__gettxt(firstltr, lastltr)
-            if text != self.spellingerrors[tag][0]:
-                self.__deltag(tag)
-                self.__settags(text, firstltr, lastltr)
+    def _firstcheck(self):
+        self._check()
 
-    def __checkline(self, evt):
-        print self.counter
-        self.__del_existing_tags(INSERT)
-        startpos = self.index('insert linestart')
-        endpos = self.index('insert lineend')
-        text = self.__gettxt(startpos, endpos)
-        self.__settags(text, startpos, endpos)
-
-    def __del_existing_tags(self, idx):
-        taglist = self.__istagged(idx)
-        for tag in taglist:
-            if tag in self.spellingerrors.keys():
-                self.__deltag(tag)
-
-    def __check_existing_tags(self, idx):
-        taglist = self.__istagged(idx)
-        for tag in taglist:
-            if tag in self.spellingerrors.keys():
-                return True
-            else:
-                return False
-
-    def __settags(self, text, startpos, endpos):
-        curpos = startpos
+    def _check(self):
+        curpos = '1.0'
+        text = self.get('1.0', END)
         self.checker.set_text(text)
         for err in self.checker:
-            firstltr = self.search(err.word, curpos, endpos)
-            lastltr = firstltr + ('+%dchars' % len(err.word))
-            found = self.__check_existing_tags(firstltr)
-            if not found:
-                tag = '{0}-{1}'.format(err.word, self.counter)
-                markleft = 'ml-{0}'.format(tag)
-                markright = 'mr-{0}'.format(tag)
-                self.tag_config(tag, foreground="red", underline=True)
-                self.mark_set(markleft, firstltr)
-                self.mark_set(markright, lastltr)
-                self.mark_gravity(markleft, LEFT)
-                self.mark_gravity(markright, RIGHT)
-                self.tag_add(tag, markleft, markright)
-                self.tag_add(tag, firstltr, lastltr)
-                self.tag_bind(tag, '<Button-3>', lambda evt,
-                              tag=tag: self.__suggestedwords(evt, tag))
+            firstpos = self.search(err.word, curpos, END)
+            lastpos = firstpos + ('+%dchars' % len(err.word))
+            tag = '{0}-{1}'.format(err.word, self.counter)
+            markleft = 'left-{0}'.format(tag)
+            markright = 'right-{0}'.format(tag)
+            self.tag_config(tag, background="yellow", foreground="red", underline=True)
+            self.mark_set(markleft, firstpos)
+            self.mark_set(markright, lastpos)
+            self.mark_gravity(markleft, LEFT)
+            self.mark_gravity(markright, RIGHT)
+            self.tag_add(tag, markleft, markright)
+            self.tag_add(tag, firstpos, lastpos)
+            self.tag_bind(tag, '<Button-3>', lambda evt,
+                          tag=tag: self._get_suggestions(evt, tag))
+            self.spellingerrors[tag] = (err.word, markleft, markright,
+                                        self.counter)
+            self.counter += 1
+            curpos = lastpos
 
-                self.spellingerrors[tag] = (err.word, markleft, markright,
-                                            self.counter)
-                self.counter += 1
-            curpos = lastltr
-
-    def __istagged(self, idx):
-        return self.tag_names(idx)
-
-    def __suggestedwords(self, evt, tag):
+    def _get_suggestions(self, evt, tag):
         err = self.spellingerrors[tag][0]
-        suggwords = self.__suggest(err)
-        context_menu = self.__contextmenu(tag, suggwords)
+        suggested = self._suggest(err)
+        context_menu = self._generate_contextmenu(tag, suggested)
         context_menu.tk_popup(evt.x_root, evt.y_root)
 
-    def __cleartags(self):
-        for tag in self.spellingerrors.keys():
-            self.__deltag(tag)
-        self.counter = 0
+    def _suggest(self, err):
+        return self.suggest_words.suggest(err)
 
-    def __clearbindings(self):
-        self.bind('<space>', lambda e: None)
+    def _generate_contextmenu(self, tag, suggested):
+        contextmenu = Menu(self, tearoff=False)
+        for word in suggested:
+            contextmenu.add_command(label=word, command=lambda tag=tag,
+                                    word=word: self._replace(tag, word))
+        return contextmenu
 
-    def __deltag(self, tag):
+    def _replace(self, tag, word):
+        firstpos, lastpos = self._tagranges(tag)
+        self.delete(firstpos, lastpos)
+        self.insert(firstpos, word)
+
+    def _tagranges(self, tag):
+        return self.tag_ranges(tag)
+
+    def _delete_tag(self, tag):
         self.tag_delete(tag)
         for i in (self.spellingerrors[tag][1], self.spellingerrors[tag][2]):
             self.mark_unset(i)
         del self.spellingerrors[tag]
 
-    def __gettxt(self, startpos=1.0, endpos=END):
-        return self.get(startpos, endpos)
-
-    def __suggest(self, err):
-        return self.suggwords.suggest(err)
-
-    def __contextmenu(self, tag, suggested):
-        err = self.spellingerrors[tag][0]
-        contextmenu = Menu(self, tearoff=False)
-        for word in suggested:
-            contextmenu.add_command(label=word, command=lambda tag=tag,
-                                    word=word: self.__replace(tag, word))
-        contextmenu.add_separator()
-        contextmenu.add_command(label='Add to Dictionary', command=lambda
-                                err=err: self.__usrdict(err))
-        return contextmenu
-
-    def __tagranges(self, tag):
-        return self.tag_ranges(tag)
-
-    def __replace(self, tag, word):
-        firstltr, lastltr = self.__tagranges(tag)
-        self.delete(firstltr, lastltr)
-        self.insert(firstltr, word)
-        self.__deltag(tag)
-
-    def __usrdict(self, err):
-        '''TODO: Add a personal dictionary file '''
-        pass
-
-if __name__ == '__main__':
-    main()
+    def _clear_tags(self):
+        for tag in self.spellingerrors.keys():
+            self._delete_tag(tag)
